f6abf7fed2e9f54bee5da5172d78e99b5a079f64
Jordan Finn <jordan.m.finn17@gmail.com>
2025-09-26 14:07:15 -0700

    Add semaphore chainlink automation

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.19;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
-// import {VRFV2PlusClient} from "@chainlinkcontracts/src/v0.8/dev/vrf/libraries/VRFV2PlusClient.sol";
 
 /**
  * @title A sample raffle contract
@@ -16,6 +15,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
     error Raffle__SendMoreToEnterRaffle();
     error Raffle__TransferFailed();
     error Raffle__RaffleNotOpen();
+    error Raffle__UpkeepNotNeeded(uint256 balance, uint256 playersLength, uint256 raffleState);
 
     // Type declarations
     enum RaffleState {
@@ -75,11 +75,43 @@ contract Raffle is VRFConsumerBaseV2Plus {
         s_players.push(payable(msg.sender));
         emit RaffleEntered(msg.sender);
     }
-
+    
+    // When should the winner be picked?
+    /**
+     * #dev This is the function that the Chainlink nodes will call to see
+     * if the lottery is ready to have a winner picked.
+     * The following should be true in order for upkeepNeeded to be true:
+     * 1. The time interval has passed between raffle runs
+     * 2. The lottery is open
+     * 3. The contract has ETH
+     * 4. Implicitly, your subscription has LINK
+     * @param - ignored
+     * @return upkeepNeeded - true if it's time to restart the lottery
+     * @return - ignored
+     */ 
+    function checkUpkeep(bytes memory /* checkData */) public view returns ( 
+        bool upkeepNeeded, 
+        bytes memory /* performData */
+    ) {
+        bool timeHasPassed = ((block.timestamp - s_lastTimeStamp) >= I_INTERVAL);
+        bool isOpen = s_raffleState == RaffleState.OPEN;
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = s_players.length > 0;
+        upkeepNeeded = timeHasPassed && isOpen && hasBalance && hasPlayers;
+        return (upkeepNeeded, "");
+    }
+        
+    // 3. Be automatically called
     // Get a random number,
     // Use that random number to pick a player,
     // Be automatically called
-    function pickWinner() external {
+    function performUpkeep(bytes calldata /* performData */) external {
+        // check to see if enough time has passed
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(address(this).balance, s_players.length, uint256(s_raffleState));
+        }
+        
         if ((block.timestamp - s_lastTimeStamp) < I_INTERVAL) {
             revert();
         }
@@ -97,24 +129,28 @@ contract Raffle is VRFConsumerBaseV2Plus {
             extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
         });
 
-        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+        s_vrfCoordinator.requestRandomWords(request);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal virtual override {
+    // CEI: Checks, Effects, Interactions Pattern
+    function fulfillRandomWords(uint256, /* requestId */ uint256[] calldata randomWords) internal virtual override {
+        // Checks
+
+        //  Effect (Internal contract states)
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
-
         // `Semaphore` status update & reset logic for next raffle
         s_raffleState = RaffleState.OPEN;
         s_players = new address payable[](0);
         s_lastTimeStamp = block.timestamp;
+        emit WinnerPicked(s_recentWinner);
 
+        // Interactions (External contract calls)
         (bool success,) = recentWinner.call{value: address(this).balance}("");
         if (!success) {
             revert Raffle__TransferFailed();
         }
-        emit WinnerPicked(s_recentWinner);
     }
 
     // Getter functions
